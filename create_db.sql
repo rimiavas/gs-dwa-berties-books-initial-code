5bafda8edd3a71e0bab1ec9c63f5e6ec75431594
rimiavas <rimiapvas@gmail.com>
2023-11-14 16:38:36 +0000

    first change

--- create_db.sql
+++ create_db.sql
@@ -1,9 +1,28 @@
 # Create database script for Berties books
 
 # Create the database
+SHOW DATABASES;
 CREATE DATABASE myBookshop;
 USE myBookshop;
 
 # Create the tables
 CREATE TABLE books (id INT AUTO_INCREMENT,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id));
+SHOW TABLES;
+DESCRIBE books;
+
+INSERT INTO books (name, price) VALUES('database book', 40.25),
+('Node.js book', 25.00), 
+('Express book', 31.99), 
+('vs code book', 1.99), 
+('my sql book', 300.99) ;
+
+SELECT * FROM books;
+
+CREATE USER 'appuser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'app2027';
+
+GRANT ALL PRIVILEGES ON myBookshop.* TO 'appuser'@'localhost';
+
+# Create the app user and give it access to the database
+CREATE USER 'appuser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'app2027';
+GRANT ALL PRIVILEGES ON myBookshop.* TO 'appuser'@'localhost';
 
--- routes/main.js
+++ routes/main.js
@@ -20,5 +20,15 @@ module.exports = function(app, shopData) {
     app.post('/registered', function (req,res) {
         // saving data in database
         res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                              
-    }); 
+    });
+    app.get('/list', function(req, res) {
+        let sqlquery = "SELECT * FROM books"; // query database to get all the books
+        // execute sql query
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                res.redirect('./'); 
+            }
+            res.send(result)
+        });
+    });
 }
